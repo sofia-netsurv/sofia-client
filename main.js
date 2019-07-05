0b4387b0f4bdf265d3f59196ed8cb61a685ddbe0
Eliot Kent Woodrich <ekwoodrich@gmail.com>
2019-07-04 20:56:49 -0700

    Updated icon directory structure

--- main.js
+++ main.js
@@ -11,6 +11,7 @@ function createWindow () {
   mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
+    backgroundColor: '#cc0096',
     webPreferences: {
       preload: path.join(__dirname, 'preload.js')
     },
