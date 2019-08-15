554ba9416e72c66601b4469c7ead29ba90151a84
plavabe12 <23537510+plavabe12@users.noreply.github.com>
2019-08-15 11:41:26 -0700

    Removed unused fills

--- app/store/configureStore.prod.js
+++ app/store/configureStore.prod.js
@@ -4,19 +4,14 @@ import thunk from 'redux-thunk';
 import { createHashHistory } from 'history';
 import { routerMiddleware } from 'connected-react-router';
 import createRootReducer from '../reducers';
-import type { counterStateType } from '../reducers/types';
 
 const history = createHashHistory();
 const rootReducer = createRootReducer(history);
 const router = routerMiddleware(history);
 const enhancer = applyMiddleware(thunk, router);
 
-function configureStore(initialState?: counterStateType) {
-  return createStore<*, counterStateType, *>(
-    rootReducer,
-    initialState,
-    enhancer
-  );
+function configureStore() {
+  return createStore(rootReducer, undefined, enhancer);
 }
 
 export default { configureStore, history };
